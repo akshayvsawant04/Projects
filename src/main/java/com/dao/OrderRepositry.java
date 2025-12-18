ca898cb0b6ecee3440ff4ad11dc018283a93538f
HP <akshayvsawant04>
2025-12-18 12:03:54 +0530

    query added

--- src/main/java/com/dao/OrderRepositry.java
+++ src/main/java/com/dao/OrderRepositry.java
@@ -1,10 +1,18 @@
 package com.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.model.Orders;
 
 public interface OrderRepositry extends JpaRepository<Orders, Long>{
 	
+	@Query("select s from Orders o where quantity > 5")
+	public List<Orders> findOrdersByQuantity();
+	
+//	@Query("select o from Orders s where s.quantity=(select max(s1.quantity) from Orders s1)")
+//	public Orders findMaxQuantityFromOrders();
 
 }
